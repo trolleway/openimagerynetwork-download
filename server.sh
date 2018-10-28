fa3db29c4a3b7504a2cd21dadff2a5148547a721
Artem Svetlov <trolleway@yandex.ru>
2018-10-28 22:39:57 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python __main__.py --storage files --last 52
-rm files/footprints.geojson
+rm -f files/footprints.geojson #-f ommit return error if file not exists 
 ogr2ogr -oo GEOM_POSSIBLE_NAMES=footprint -oo KEEP_GEOM_COLUMNS=NO -s_srs EPSG:4326 -t_srs EPSG:3857 files/footprints.geojson files/footprints.csv
